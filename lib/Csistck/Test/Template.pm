aaf5779241a21e54f3e937f88a9e133df74484a6
Anthony Johnson <agj@cpan.org>
2012-06-18 06:27:12 +0000

    Import of AGJ/Csistck-0.10 from CPAN.
    gitpan-cpan-distribution: Csistck
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         AGJ/Csistck-0.10.tar.gz
    gitpan-cpan-author:       AGJ
    gitpan-cpan-maturity:     released

--- lib/Csistck/Test/Template.pm
+++ lib/Csistck/Test/Template.pm
@@ -94,12 +94,50 @@ Csistck::Test::Template - Csistck template check
 
 =head1 METHODS
 
-=head2 template($template, $target, [%args])
+=head2 template($target, :$src, :$uid, :$gid, :\&on_repair, [:ARGS])
 
-Process template toolkit file and output to target path. Target
-path should be a file in an existing path. 
+Process file C<$src> as a Template Toolkit template, output to path C<$target>.
+Optional named arguments can be used to alter the mode, uid, etc. All parameters
+passed into the C<Csistck::Test::Template> object are available in the actual
+template, so any additional named arguments are available in the template using
+the argument's name -- these arguments should be hasrefs.
 
-    role 'test' => template('sys/motd.tt', '/etc/motd', { production => 1 });
+    role 'test' => template(
+        '/etc/motd',
+        src => 'sys/motd',
+        foo => { bar => 1 },
+        uid => 0,
+        gid => 0,
+        mode => '0640'
+    );
+
+This method takes the following named parameters, as well as additional
+named parameters to be passed in to the template as variables:
+
+=over
+
+=item B<src>
+
+Source file for template processing
+
+=item B<mode>
+
+Change targer file mode. This should be a string representation of the octal
+mode of the target file -- eg. '0644'
+
+=item B<uid>
+
+Change target UID to the specified integer value.
+
+=item B<gid>
+
+Change target GID to the specified integer value.
+
+=item B<on_repair>
+
+If a repair operation is run, this coderef is called by the process method.
+
+=back
 
 Some arguments are automatically passed to the template processor:
 
@@ -117,7 +155,7 @@ Anthony Johnson, E<lt>anthony@ohess.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2011 Anthony Johnson
+Copyright (c) 2011-2012 Anthony Johnson
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -138,5 +176,3 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 =cut
-
-
